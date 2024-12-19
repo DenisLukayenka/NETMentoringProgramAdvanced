6145482192ddd301324a8cf8696d36d8675670d1
Dzianis.Lukayenka <dzianis.lukayenka@phreesia.com>
2024-12-19 21:30:44 +0100

    add basic tests

---
+++ src/Catalog.Service/ApplicationCore.Test/Categories/Commands/DeleteCategoryTests.cs
@@ -0,0 +1,102 @@
+﻿using ApplicationCore.Categories.Commands.DeleteCategory;
+using ApplicationCore.Common.Exceptions;
+
+namespace ApplicationCore.Test.Categories.Commands;
+
+[TestFixture]
+internal class DeleteCategoryTests
+{
+    private Mock<ICategoryRepository> _repositoryMock;
+    private Fixture _fixture;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _repositoryMock = new Mock<ICategoryRepository>();
+        _fixture = new Fixture();
+
+        _fixture.Customize<Category>(builder =>
+            builder
+                .With(c => c.Name, _fixture.Create<string>())
+                .With(c => c.Image, _fixture.Create<string>())
+                .Without(c => c.ParentCategoryId)
+                .Without(c => c.ParentCategory)
+                .With(c => c.ChildCategories, []));
+    }
+
+    private DeleteCategoryCommandHandler CreateHandler()
+        => new(_repositoryMock.Object);
+
+    [Test]
+    public async Task Handle_ShouldDeleteCategory_WhenCategoryExistsWithoutDependents()
+    {
+        // Arrange
+        var category = _fixture.Create<Category>();
+        var command = new DeleteCategoryCommand(category.Id);
+
+        _repositoryMock
+            .Setup(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
+
+        _repositoryMock
+            .Setup(repo => repo.List(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync([]);
+
+        var handler = CreateHandler();
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        _repositoryMock.Verify(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.List(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Delete(category, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public void Handle_ShouldThrowEntityNotFoundException_WhenCategoryDoesNotExist()
+    {
+        // Arrange
+        var command = new DeleteCategoryCommand(1);
+
+        _repositoryMock
+            .Setup(repo => repo.Get(1, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Category)null);
+
+        var handler = CreateHandler();
+
+        // Act & Assert
+        FluentActions.Invoking(() => handler.Handle(command, CancellationToken.None))
+            .Should().ThrowAsync<EntityNotFoundException>();
+
+        _repositoryMock.Verify(repo => repo.Get(1, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Delete(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Test]
+    public async Task Handle_ShouldDeleteCategory_WhenCategoryExistsWithDependents()
+    {
+        // Arrange
+        var category = _fixture.Create<Category>();
+        var dependents = _fixture.CreateMany<Category>(3).ToArray();
+        var command = new DeleteCategoryCommand(category.Id);
+
+        _repositoryMock
+            .Setup(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
+
+        _repositoryMock
+            .Setup(repo => repo.List(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(dependents);
+
+        var handler = CreateHandler();
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        _repositoryMock.Verify(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.List(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Delete(category, It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ src/Catalog.Service/ApplicationCore.Test/Categories/Commands/UpdateCategoryTests.cs
@@ -0,0 +1,121 @@
+﻿using ApplicationCore.Categories.Commands.UpdateCategory;
+using ApplicationCore.Common.Exceptions;
+
+namespace ApplicationCore.Test.Categories.Commands;
+
+[TestFixture]
+public class UpdateCategoryTests
+{
+    private Mock<ICategoryRepository> _repositoryMock;
+    private Fixture _fixture;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _repositoryMock = new Mock<ICategoryRepository>();
+        _fixture = new Fixture();
+
+        _fixture.Customize<Category>(builder =>
+            builder
+                .With(c => c.Name, _fixture.Create<string>())
+                .With(c => c.Image, _fixture.Create<string>())
+                .Without(c => c.ParentCategoryId)
+                .Without(c => c.ParentCategory)
+                .With(c => c.ChildCategories, []));
+    }
+
+    private UpdateCategoryCommandHandler CreateHandler()
+        => new(_repositoryMock.Object);
+
+    [Test]
+    public async Task Handle_ShouldUpdateCategory_WhenValidRequestWithNameAndImage()
+    {
+        // Arrange
+        var category = _fixture.Create<Category>();
+        var command = new UpdateCategoryCommand(category.Id, "Updated Name", "Updated Image");
+
+        _repositoryMock
+            .Setup(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
+
+        var handler = CreateHandler();
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        category.Name.Should().Be("Updated Name");
+        category.Image.Should().Be("Updated Image");
+
+        _repositoryMock.Verify(repo => repo.Get(command.CategoryId, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Update(category, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public async Task Handle_ShouldUpdateCategory_WhenValidRequestWithNameOnly()
+    {
+        // Arrange
+        var category = _fixture.Create<Category>();
+        var command = new UpdateCategoryCommand(category.Id, "Updated Name", null);
+
+        _repositoryMock
+            .Setup(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
+
+        var handler = CreateHandler();
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        category.Name.Should().Be("Updated Name");
+        category.Image.Should().Be(category.Image);
+
+        _repositoryMock.Verify(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Update(category, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public async Task Handle_ShouldUpdateCategory_WhenValidRequestWithImageOnly()
+    {
+        // Arrange
+        var category = _fixture.Create<Category>();
+        var command = new UpdateCategoryCommand(category.Id, null, "Updated Image");
+
+        _repositoryMock
+            .Setup(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
+
+        var handler = CreateHandler();
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        category.Name.Should().Be(category.Name);
+        category.Image.Should().Be("Updated Image");
+
+        _repositoryMock.Verify(repo => repo.Get(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Update(category, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public void Handle_ShouldThrowEntityNotFoundException_WhenCategoryDoesNotExist()
+    {
+        // Arrange
+        var command = _fixture.Create<UpdateCategoryCommand>();
+
+        _repositoryMock
+            .Setup(repo => repo.Get(command.CategoryId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Category)null);
+
+        var handler = CreateHandler();
+
+        // Act & Assert
+        FluentActions.Invoking(() => handler.Handle(command, CancellationToken.None))
+            .Should().ThrowAsync<EntityNotFoundException>();
+
+        _repositoryMock.Verify(repo => repo.Get(command.CategoryId, It.IsAny<CancellationToken>()), Times.Once);
+        _repositoryMock.Verify(repo => repo.Update(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+}
